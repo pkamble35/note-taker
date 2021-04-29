9e0a39204556a28f8da7f1029164b2642a49ac61
priyanka damodar <priyadamodar12@gmail.com>
2021-04-28 23:52:25 -0400

    fixed deployment issue with port

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express'),
-	app = express(),
-	port = 8080;
+	app = express();
 app.use(express.static('public'));
 app.use(express.json());
 var routes = require('./src/routes/notesTakerRoutes'); //importing route
 
 routes(app); // registering the routes from the routes file
-
-
-app.listen(port);
-
-console.log('Notes Taker is now running on port test : ' + port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
